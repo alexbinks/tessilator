2608f70e106f321a1262f4e4216c3a83a36924e8
alexbinks <alex.s.binks@gmail.com>
2023-03-06 19:54:58 -0500

    Adding the run_tess files

--- run_tess_large_sectors.py
+++ run_tess_large_sectors.py
@@ -1,25 +1,28 @@
-from TESSilator import tess_cutouts, tess_functions2
+from TESSilator import tessilator
 import numpy as np
 import os
+import logging
 from astropy.table import join
 from astropy.io import ascii
 
-fluxCon, secNum, makePlots, fileRef, tFilename = tess_cutouts.setupInputParameters()
-conFile, periodFile, storeFile = tess_cutouts.setupFilenames(fileRef, sector_num=secNum)
+logging.basicConfig(filename="output.log", level=logging.ERROR)
 
+
+fluxCon, scc, makePlots, fileRef, tFilename = tess_cutouts.setupInputParameters()
+conFile, periodFile = tess_cutouts.setupFilenames(fileRef, scc=scc)
 t_large_sec_check = tess_cutouts.test_table_large_sectors(tFilename)
 
 if t_large_sec_check is not None:
     tTargets = t_large_sec_check
 else:
-    gaia_data = tess_cutouts.readData(tFilename, storeFile=storeFile, name_is_source_id=1)
-    xy_pixel_data = tess_functions2.getTESSPixelXY(gaia_data)
+    gaia_data = tess_cutouts.readData(tFilename, name_is_source_id=1)
+    xy_pixel_data = tess_cutouts.getTESSPixelXY(gaia_data)
     tTargets = join(gaia_data, xy_pixel_data, keys='source_id')
-ascii.write(tTargets, tFilename, format='csv', overwrite=True)
+    ascii.write(tTargets, tFilename, format='csv', overwrite=True)
 
-tTargets = tTargets[tTargets['Sector'] == secNum]
+tTargets = tTargets[tTargets['Sector'] == scc[0]]
 Rad, SkyRad = 1.0, np.array([6.0,8.0])
 
 tTargets = tess_cutouts.collectContaminationData(tTargets, fluxCon, 0, conFile, Rad=Rad)
 
-tess_cutouts.iterate_all_cc(tTargets, storeFile, secNum, makePlots, periodFile)
+tess_cutouts.iterate_all_cc(tTargets, scc, makePlots, periodFile)
