fb69c1f6a984a9845d5611807c5df9f77c238177
alexbinks <alex.s.binks@gmail.com>
2023-03-06 19:54:58 -0500

    Adding the run_tess files

--- run_tess_cutouts.py
+++ run_tess_cutouts.py
@@ -1,21 +1,22 @@
-from TESSilator import tess_cutouts
+from TESSilator import tessilator
 
 import numpy as np
+import logging
 
-
-flux_con, LC_con, make_plots, file_ref, t_filename = tess_cutouts.setupInputParameters()
-con_file, period_file, store_file = tess_cutouts.setupFilenames(file_ref)
+fluxCon, lcCon, makePlots, fileRef, tFilename = tessilator.setupInputParameters()
+conFile, periodFile = tessilator.setupFilenames(fileRef)
 
 Rad, SkyRad = 1.0, np.array([6.0,8.0])
 
+logging.basicConfig(filename="output.log", level=logging.WARNING)
 
 print("Reading the table and formatting into astropy table structure.")
-t_targets = tess_cutouts.readData(t_filename, store_file=store_file)
+tTargets = tessilator.readData(tFilename)
 print("Done reading the table and formatting.")
 
 print("...now calculating the contamination.")
-t_targets = tess_cutouts.collectContaminationData(t_targets, flux_con, LC_con, con_file, Rad=Rad)
+tTargets = tessilator.collectContaminationData(tTargets, fluxCon, lcCon, conFile, Rad=Rad)
 print("Done calculating the contamination.")
 
 print("...now iterating over each source.")
-tess_cutouts.iterate_sources(t_targets, period_file, store_file, LC_con, flux_con, con_file, make_plots)
+tessilator.all_sources_cutout(tTargets, periodFile, lcCon, fluxCon, conFile, makePlots, choose_sec=36)
