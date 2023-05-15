9c61fbe13fbe1557d54e6279406035f6b427c4bd
alexbinks <alex.s.binks@gmail.com>
2023-05-15 19:56:03 -0400

    changed the tests a little

--- tessilator/tests/test_clean_lc.py
+++ tessilator/tests/test_clean_lc.py
@@ -11,14 +11,14 @@ times = np.linspace(start=start+typical_timestep, stop=stop, num=int(stop/typica
 
 def test_flat():
     '''TRY A COMPLETELY FLAT LIGHTCURVE'''
-    x, y = clean_lc(times, np.ones(len(times)))
+    x, y = clean_lc(times, np.ones(len(times)), np.zeros(len(times)))
     assert(x[0] == 0)
     assert(y[0] == len(times)-1)
 
 
 def test_sine():
     '''TRY A PERFECTLY SINUSOIDAL LIGHTCURVE'''
-    x, y = clean_lc(times, 1.0 + 0.1*np.sin(2.*math.pi*times/period))
+    x, y = clean_lc(times, 1.0 + 0.1*np.sin(2.*math.pi*times/period), np.zeros(len(times)))
     assert(x[0] == 0)
     assert(y[0] == len(times)-1)
 
@@ -59,7 +59,7 @@ def test_simulated():
     i3 = i + sum(LCpart == 0)
 
 
-    x, y = clean_lc(times_fin, flux_fin, MAD_fac=MAD_test, time_fac=10., min_num_per_group=50)
+    x, y = clean_lc(times_fin, flux_fin, np.zeros(len(times_fin)), MAD_fac=MAD_test, time_fac=10., min_num_per_group=50)
 
     assert(x[0] == i0)
     assert(y[0] == i1)
